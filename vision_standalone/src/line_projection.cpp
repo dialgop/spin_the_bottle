63d907a8a14f9d5bb941f941b0c4c39da6a9f8a9
dialgop <dialgop@gmail.com>
2026-07-28 03:55:34 +0200

    Create draw_pointin_line method in line_projection.cpp

--- vision_standalone/src/line_projection.cpp
+++ vision_standalone/src/line_projection.cpp
@@ -17,9 +17,28 @@ std::optional<PointingLine> compute_pointing_line(const cv::Mat& bgr_image, cons
     return {};
 }
 
-void draw_pointing_line(cv::Mat& image, const PointingLine& line)
-{
-    return;
-}
+    void draw_pointing_line(cv::Mat& image, const PointingLine& line)
+    {
+        cv::ellipse(image, line.ellipse, cv::Scalar(180), 2, 8);
+
+        const cv::Point center(cv::saturate_cast<int>(line.ellipse.center.x), cv::saturate_cast<int>(line.ellipse.center.y));
+        const cv::Point endpoint(cv::saturate_cast<int>(line.line_end.x), cv::saturate_cast<int>(line.line_end.y));
+        const double angle_rad = line.angle_degrees * M_PI / 180;
+
+        cv::line(image, center, endpoint, cv::Scalar(200), 4);
+
+        // Two short "hook" lines near the tip, angled +/-45 degrees off the main
+        // line, to draw an arrowhead shape.
+        const cv::Point hook1(
+            cv::saturate_cast<int>(endpoint.x - 12 * std::cos(angle_rad + M_PI / 4)),
+            cv::saturate_cast<int>(endpoint.y - 12 * std::sin(angle_rad + M_PI / 4)));
+        cv::line(image, hook1, endpoint, cv::Scalar(200), 4);
+
+        const cv::Point hook2(
+            cv::saturate_cast<int>(endpoint.x - 12 * std::cos(angle_rad - M_PI / 4)),
+            cv::saturate_cast<int>(endpoint.y - 12 * std::sin(angle_rad - M_PI / 4)));
+        cv::line(image, hook2, endpoint, cv::Scalar(200), 4);
+    }
+
 
 }
